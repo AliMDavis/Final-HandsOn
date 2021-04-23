cae2bb26bc15dd0b37e8315e02684f8e87b1f9f3
AliMDavis <ad767218@ohio.edu>
2021-04-22 20:21:16 -0400

    created count.cc file

---
+++ count.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+
+
+using namespace std;
+
+
+int countLine(string text);
+int countChar(string text);
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
