9a6f9e7409d46869a226c067ab7e66120e65eb2c
AliMDavis <ad767218@ohio.edu>
2021-04-22 20:41:24 -0400

    countChar() function added

--- count.cc
+++ count.cc
@@ -2,15 +2,33 @@
 #include <cstdlib>
 #include <fstream>
 #include <string>
+#include <cctype>
+#include <stdio.h>
 
 
 using namespace std;
 
 
-int countLine(string text);
-int countChar(string text);
-
-int main(int argc, char *argv[])
+int countLine(string text)
 {
+    int lineCounter;
+
+    
+}
+
+
+int countChar(char *text)
+{
+    int charCounter;
+    string s = text;
+        charCounter = s.size();
+
+    return charCounter;
+
+}
+
+int main(int argc, char **argv)
+{
+    cout << countChar(argv[1]) << " Lines" << endl;
     return 0;
 }
